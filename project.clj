333d5a675931cb7bce3e418966635fe6040a8408
Josh Jahng <josh@Joshs-MacBook-Pro.local>
2017-12-11 20:39:13 -0500

    Added Spark

--- project.clj
+++ project.clj
@@ -5,8 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [spyscope "0.1.6"]
+                 [yieldbot/flambo "0.8.2"]
                  [iota "1.1.3"]
                  [digest "1.4.6"]]
   :main ^:skip-aot file-checker.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:provided
+             {:dependencies
+              [[org.apache.spark/spark-core_2.11 "2.2.0"]]}
+             :dev {:aot [file-checker.core]}
+             :uberjar {:aot :all}})
--- src/file_checker/core.clj
+++ src/file_checker/core.clj
@@ -2,16 +2,22 @@
   (:require [clojure.java.io :as io]
             [digest]
             [iota]
+            [file-checker.spark-context :as sc]
+            [flambo.api :as f]
+            [flambo.tuple :as ft]
             [clojure.core.reducers :as r]
             [clojure.pprint :refer [pprint]])
-  (:gen-class))
+  (:gen-class)
+  (:import [org.apache.spark.api.java JavaRDD]))
 
-(declare file->map dir->map duplicates)
+(declare file->map dir->files parallel duplicates duplicated-files)
 
 (defn -main
   [& args]
   (println "Looking for duplicate files ..")
-  (->> args first dir->map duplicates))
+  (->> args first dir->files parallel duplicated-files)
+  ;;(->> args first dir->files duplicates)
+  )
 
 (defn- file->map
   "Returns hash map of a key and the file path.
@@ -20,19 +26,55 @@
   [f & {:keys [algorithm] :or {algorithm digest/sha}}]
   (hash-map (algorithm f) (.getPath f)))
 
-(defn dir->map
+(f/defsparkfn f->m
+  [f]
+  (ft/tuple (digest/sha f) (.getPath f)))
+
+(defn dir->files
   "Returns a tree made out of the given directory"
   [dir]
   (->> dir
        io/file
        file-seq
-       (r/filter #(.isFile %))))
+       (filter #(.isFile %))))
 
-(defn- tr
+(defn- duplicated?
+  [rdd]
+  (when (<= 2 (count (._2 rdd))) true))
+
+(defn- parallel
+  "expecting dir->map's output as a input"
+  [fs]
+  (let [sc (if (= org.apache.spark.api.java.JavaSparkContext (class @sc/sc))
+             @sc/sc
+             (throw (Exception. "Set up Spark context first. Call (set-sc)")))]
+    (-> (f/parallelize @sc/sc fs)
+        (f/map-to-pair f->m)
+        f/group-by-key
+        (f/filter duplicated?)
+        (f/map (fn [x] (._2 x)))
+        f/collect
+        )))
+
+(defn- duplicated-files
+  "Returns duplicated files from a given Spark's result"
+  [result]
+  (->> result
+       (map #(take 2 %))
+       (into [])))
+
+(defn- trx
   "Returns a tree of the key and the file path pairs."
   [ms]
   (r/fold ;; Parallel reducing
-   (fn dd
+   ;; combining step. {K [Vs ..]} is a input not {k v}
+   (fn cc
+     ([] {})
+     ([ret x]
+      (let [k (-> x keys first)]
+        (assoc ret k (conj (get ret k []) x)))))
+   ;; reducing step. create hash map of {k [vs..]}
+   (fn rr
      ([] {})
      ([ret x]
       (let [kv (file->map x)
@@ -41,9 +83,11 @@
    ms))
 
 (defn- duplicates
+  "Returns duplicated files from a given folding result"
   [m]
   (->> m
-       tr
+       (pmap file->map)
+       trx
        (filter (fn ff [[k v]] (<= 2 (count v))))
        (map (fn [[k v]] (mapcat vals v)))
        (into [])))
